e9e0a76d4ee2558d5579f20b72cade0d06fd336e
burbage stacey <rstacey06@gmail.com>
2019-10-25 19:12:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,22 @@
-# Write your code here!
+def game_hash
+  {
+    home: "Brooklyn Nets",
+    colors: ["black","white"],
+    players:[
+      {
+       player_name: "Alan Anderson",
+       number: 0,
+       shoe: 16,
+       points: 22,
+       rebounds 12,
+       assists 12,
+       steals: 3,
+       blocks: 1,
+       dunks: 1,
+      },
+      {
+    
+  }
 
 
 
